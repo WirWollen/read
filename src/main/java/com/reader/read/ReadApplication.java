3408d7d0e77f2d6faa52d1bb029a1503e79bdd27
WirWollen <52575418+WirWollen@users.noreply.github.com>
2023-08-25 23:36:19 +0400

    Initial commit

---
+++ src/main/java/com/reader/read/ReadApplication.java
@@ -0,0 +1,13 @@
+package com.reader.read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reader/read/ReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reader.read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
